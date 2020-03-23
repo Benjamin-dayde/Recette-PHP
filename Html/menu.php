3dcc6e0f9e2e724a737af517a53313af21e10641
Benjamin-dayde <benjamin.dayde@orange.fr>
2020-03-23 09:27:13 +0100

    modification des lien

--- Html/menu.php
+++ Html/menu.php
@@ -10,25 +10,25 @@
 <nav class="menu">
                 <ul>
                     <li><a href ="#">Page d'accueil</a></li>
-                    <li><a href ="recettes/entree.html">Les entrées</a>
+                    <li><a href ="index.php?page=entree">Les entrées</a>
                         <ul class="sous-menu">
                             <li><a href ="index.php?page=entree1">Entrée 1</a></li>
                             <li><a href ="index.php?page=entree2">Entrée 2</a></li>
                             <li><a href ="index.php?page=entree3">Entrée 3</a></li>
                         </ul>
                     </li>
-                    <li><a href ="recettes/plat.html">Les plats</a>
+                    <li><a href ="index.php?page=plat">Les plats</a>
                         <ul class="sous-menu">
-                            <li><a href ="recettes/plats/plat1.html">Plat 1</a></li>
-                            <li><a href ="recettes/plats/plat2.html">Plat 2</a></li>
-                            <li><a href ="recettes/plats/plat3.html">Plat 3</a></li>
+                            <li><a href ="index.php?page=plat1">Plat 1</a></li>
+                            <li><a href ="index.php?page=plat2">Plat 2</a></li>
+                            <li><a href ="index.php?page=plat3">Plat 3</a></li>
                         </ul>
                     </li>
-                    <li><a href ="recettes/dessert.html">Les desserts</a>
+                    <li><a href ="index.php?page=dessert">Les desserts</a>
                         <ul class="sous-menu">
-                            <li><a href ="recettes/desserts/dessert1.html">Dessert 1</a></li>
-                            <li><a href ="recettes/desserts/dessert2.html">Dessert 2</a></li>
-                            <li><a href ="recettes/desserts/dessert3.html">Dessert 3</a></li>
+                            <li><a href ="index.php?page=dessert1">Dessert 1</a></li>
+                            <li><a href ="index.php?page=dessert2">Dessert 2</a></li>
+                            <li><a href ="index.php?page=dessert3">Dessert 3</a></li>
                         </ul>
                     </li>
                 </ul>
