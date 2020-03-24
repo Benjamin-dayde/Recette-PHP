d8dfe175ff13e90c6463f81ded3dd679878dbfc8
Benjamin-dayde <benjamin.dayde@orange.fr>
2020-03-24 08:10:05 +0100

    Tableaux imbriqué

--- Recettes.php
+++ Recettes.php
@@ -2,14 +2,49 @@
 
 $recettes_test = [
     "titre" => "Plat 2",
-    "image" => "../../img/plat2.jpg",
+    "image" => "img\plat2.jpg",
     "temps" => "2h05",
     "personnes" => "8",
     "dificulte" => "3/4",
     "cout" => "3/4",
     "ingredient" => "1 paquet de lazagne /// 3 oignon jaune /// 3 gousse d'ails /// 1 brnache de celerie /// 1 caroote /// etc",
     "etape" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive. /// Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout. /// Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation.",
+];
 
+$cat_test = [
+
+    $recettes1 = [
+        "titre" => "Plat 2",
+    "image" => "img\plat2.jpg",
+    "temps" => "2h05",
+    "personnes" => "8",
+    "dificulte" => "3/4",
+    "cout" => "3/4",
+    "ingredient" => "1 paquet de lazagne /// 3 oignon jaune /// 3 gousse d'ails /// 1 brnache de celerie /// 1 caroote /// etc",
+    "etape" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive. /// Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout. /// Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation.",
+    ],
+
+    $recettes2 = [
+        "titre" => "Plat 2",
+    "image" => "img\plat2.jpg",
+    "temps" => "2h05",
+    "personnes" => "8",
+    "dificulte" => "3/4",
+    "cout" => "3/4",
+    "ingredient" => "1 paquet de lazagne /// 3 oignon jaune /// 3 gousse d'ails /// 1 brnache de celerie /// 1 caroote /// etc",
+    "etape" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive. /// Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout. /// Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation.",
+    ],
+
+    $recettes3 = [
+        "titre" => "Plat 2",
+    "image" => "img\plat2.jpg",
+    "temps" => "2h05",
+    "personnes" => "8",
+    "dificulte" => "3/4",
+    "cout" => "3/4",
+    "ingredient" => "1 paquet de lazagne /// 3 oignon jaune /// 3 gousse d'ails /// 1 brnache de celerie /// 1 caroote /// etc",
+    "etape" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive. /// Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout. /// Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation.",
+    ]
 ];
 
 //var_dump($recettes_test)
@@ -56,6 +91,7 @@ for ($i == 0 ; $i < $size ; $i++) {
 
 <body>
     <h1><?= $recettes_test["titre"] ?>
+    <img src="<?= $recettes_test["image"] ?>"alt="Image plat 2"></a>
     <table>
         <tr>
         <th>Temps de preparation </th>
