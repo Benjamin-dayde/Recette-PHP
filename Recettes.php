b1bde395a5685cdf19d988144def51c478fcb909
Benjamin-dayde <benjamin.dayde@orange.fr>
2020-03-24 10:10:59 +0100

    Modification des tableaux

--- Recettes.php
+++ Recettes.php
@@ -1,112 +1,12 @@
-<?php 
+<?php
 
-$recettes_test = [
-    "titre" => "Plat 2",
-    "image" => "img\plat2.jpg",
-    "temps" => "2h05",
-    "personnes" => "8",
-    "dificulte" => "3/4",
-    "cout" => "3/4",
-    "ingredient" => "1 paquet de lazagne /// 3 oignon jaune /// 3 gousse d'ails /// 1 brnache de celerie /// 1 caroote /// etc",
-    "etape" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive. /// Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout. /// Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation.",
+$recette_test = [
+    "titre" => "Entree 1",
+    "image" => "entree1.jpg",
+    "temps" => "15sec",
+    "personnes" => 8,
+    "difficulte" => 1,
+    "cout" => 1,
+    "ingredients" => "1 paquet de pâtes de lasagnes///3 oignons jaunes///3 gousses d'ail///1 branche de céleri///1 carotte</li>",
+    "etapes" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive.///Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout.///Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation."
 ];
-
-$cat_test = [
-
-    $recettes1 = [
-        "titre" => "Plat 2",
-    "image" => "img\plat2.jpg",
-    "temps" => "2h05",
-    "personnes" => "8",
-    "dificulte" => "3/4",
-    "cout" => "3/4",
-    "ingredient" => "1 paquet de lazagne /// 3 oignon jaune /// 3 gousse d'ails /// 1 brnache de celerie /// 1 caroote /// etc",
-    "etape" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive. /// Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout. /// Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation.",
-    ],
-
-    $recettes2 = [
-        "titre" => "Plat 2",
-    "image" => "img\plat2.jpg",
-    "temps" => "2h05",
-    "personnes" => "8",
-    "dificulte" => "3/4",
-    "cout" => "3/4",
-    "ingredient" => "1 paquet de lazagne /// 3 oignon jaune /// 3 gousse d'ails /// 1 brnache de celerie /// 1 caroote /// etc",
-    "etape" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive. /// Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout. /// Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation.",
-    ],
-
-    $recettes3 = [
-        "titre" => "Plat 2",
-    "image" => "img\plat2.jpg",
-    "temps" => "2h05",
-    "personnes" => "8",
-    "dificulte" => "3/4",
-    "cout" => "3/4",
-    "ingredient" => "1 paquet de lazagne /// 3 oignon jaune /// 3 gousse d'ails /// 1 brnache de celerie /// 1 caroote /// etc",
-    "etape" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive. /// Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout. /// Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation.",
-    ]
-];
-
-//var_dump($recettes_test)
-
-
-
-
-
-
-
-/*$size = sizeof($recettes_test)
-
-$titre = "";
-$image = "";
-$temps = "";
-$personnes = "";
-$dificult = "";
-$cout = ""; 
-$ingredient = "";
-$etape = ""; 
-
-for ($i == 0 ; $i < $size ; $i++) {
-    
-    $titre = "";
-    $image = "";
-    $temps = "";
-    $personnes = "";
-    $dificult = "";
-    $cout = ""; 
-    $ingredient = "";
-    $etape = ""; 
-}*/
-
-?>
-
-<!DOCTYPE html>
-<html lang="fr">
-
-    <head>
-        <title>Mon livre de recettes</title>
-        <meta charset="utf-8">
-         <link rel="stylesheet" type="text/css" href="css/style.css">
-    </head>
-
-<body>
-    <h1><?= $recettes_test["titre"] ?>
-    <img src="<?= $recettes_test["image"] ?>"alt="Image plat 2"></a>
-    <table>
-        <tr>
-        <th>Temps de preparation </th>
-        <th>Personnes</th>
-        <th>Difficulté</th>
-        <th>Coût</th>
-        </tr>
-        <tr>
-        <td><?= $recettes_test["temps"] ?></td>
-        <td><?= $recettes_test["personnes"] ?></td>
-        <td><?= $recettes_test["dificulte"] ?></td>
-        <td><?= $recettes_test["cout"] ?></td>
-        
-        </tr>
-        </table>
-</body>
-
-</html>
